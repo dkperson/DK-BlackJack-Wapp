f9a06552f4979ca1b6c5cbb5410975525d99a70f
dkperson <dkperson777@gmail.com>
2014-03-29 23:21:53 -0400

    Version 1.0.1

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,4 @@ class User < ActiveRecord::Base
   has_many :credentials
   validates :name, presence: true, length: {maximum: 30}, uniqueness: {case_sensitive: false}
   validates :password, presence: true, length: {minimum: 6}
-
 end
